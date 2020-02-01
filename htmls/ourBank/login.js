01e9619c00f85dc39aab34ab59cf10cf8f0037fd
maze1943 <louyi001@outlook.com>
2020-02-02 03:15:20 +0800

    修复简单问题

--- htmls/ourBank/login.js
+++ htmls/ourBank/login.js
@@ -15,6 +15,9 @@
             case "mixiujuan":
                 errorInfo = "嗯，很接近了，加油";
                 break;
+            case "小麋鹿":
+                errorInfo = "聪明哦，想看星星嘛？输入‘小星星’试试";
+                break;
             default:
                 errorInfo = "";
         }
@@ -24,8 +27,13 @@
         const wrapEle = document.querySelector('.wrap'), account = document.querySelector('.account'), password = document.querySelector('.password');
         let Node = '';
         if (account['value'] === "小麋鹿" && password['value'] === "扑通扑通") {
-            alert('1');
             window['goto']("accountInfo");
+        }
+        else if (account['value'] === "小星星" && password['value'] === "扑通扑通") {
+            window['goto']("starNight");
+        }
+        else {
+            infoArea.innerHTML = "啊哦，登鹿失败了";
         }
     }
     _g = {
--- htmls/ourBank/login.ts
+++ htmls/ourBank/login.ts
@@ -18,6 +18,9 @@
             case "mixiujuan":
                 errorInfo = "嗯，很接近了，加油";
                 break;
+            case "小麋鹿":
+                errorInfo = "聪明哦，想看星星嘛？输入‘小星星’试试";
+                break;
             default:
                 errorInfo = "";
         }
@@ -30,8 +33,11 @@
             password = document.querySelector('.password');
         let Node = '';
         if (account['value'] === "小麋鹿" && password['value'] === "扑通扑通") {
-            alert('1');
             window['goto']("accountInfo");
+        }else if (account['value'] === "小星星" && password['value'] === "扑通扑通"){
+            window['goto']("starNight");
+        }else{
+            infoArea.innerHTML = "啊哦，登鹿失败了";
         }
     }
     _g = {
